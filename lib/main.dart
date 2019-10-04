2fa64757cad78e4250b8d95c891afc9ec3c0d03c
AshishTaduce <ashishteddy2@gmail.com>
2019-10-04 10:47:32 +0530

    Done Xylophone

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XyloPhone());
-
-
+AudioPlayer audioPlayer = AudioPlayer();
 
 class XyloPhone extends StatelessWidget {
   @override
@@ -11,10 +12,112 @@ class XyloPhone extends StatelessWidget {
       home: Scaffold(
         body: SafeArea(
           child: Center(
-            child: FlatButton(
-              color: Colors.purple.shade300,
-              child: Text('Play Sound'),
-              onPressed: (){},
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note1.wav');
+                    },
+                    child: Container(
+                      color: Colors.red,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note2.wav');
+                    },
+                    child: Container(
+                      color: Colors.orange,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note3.wav');
+                    },
+                    child: Container(
+                      color: Colors.yellow,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note4.wav');
+                    },
+                    child: Container(
+                      color: Colors.green,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note5.wav');
+                    },
+                    child: Container(
+                      color: Colors.lightBlueAccent,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note6.wav');
+                    },
+                    child: Container(
+                      color: Colors.indigo,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: InkWell(
+                    onTap: () {
+                      AudioCache player = AudioCache();
+                      player.play('note7.wav');
+                    },
+                    child: Container(
+                      color: Colors.purple,
+
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
@@ -22,4 +125,3 @@ class XyloPhone extends StatelessWidget {
     );
   }
 }
-
