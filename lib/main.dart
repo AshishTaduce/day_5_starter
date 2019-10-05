3d87b1147858a6f0f62a22e0143e2362ce613f7f
AshishTaduce <ashishteddy2@gmail.com>
2019-10-05 15:48:20 +0530

    Done Xylophone with Randomizer

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
+import 'dart:math';
+import 'package:random_color/random_color.dart';
 
 void main() => runApp(XyloPhone());
 AudioPlayer audioPlayer = AudioPlayer();
+
+Widget xylo() {
+  RandomColor _randomColor = RandomColor();
+  Color _color = _randomColor.randomColor(
+      colorBrightness: ColorBrightness.light,
+  );
+  final _random = new Random();
+  int sound(int min, int max) => min + _random.nextInt(max - min);
+  Expanded c = Expanded(
+    flex: 1,
+    child: InkWell(
+      onTap: () {
+        AudioCache player = AudioCache();
+        player.play('note${sound(1, 8)}.wav');
+      },
+      child: Container(
+        color: _color,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+      ),
+    ),
+  );
+  return c;
+}
+
+List initiate() {
+  List size = [1, 2, 3, 4, 5, 6, 7];
+  List widgets = [];
+  for (int x in size) {
+    widgets.add(xylo());
+  }
+  return widgets;
+}
+
+Widget build(BuildContext context) {
+  return MaterialApp(
+    home: Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: InkWell(
+                  onTap: () {
+                    AudioCache player = AudioCache();
+                    player.play('note1.wav');
+                  },
+                  child: Container(
+                    color: Colors.red,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: InkWell(
+                  onTap: () {
+                    AudioCache player = AudioCache();
+                    player.play('note2.wav');
+                  },
+                  child: Container(
+                    color: Colors.orange,
+
+//                        AudioCache player = AudioCache();
+//                        player.play('note1.wav');
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ),
+  );
+}
 
 class XyloPhone extends StatelessWidget {
   @override
@@ -14,109 +95,13 @@ class XyloPhone extends StatelessWidget {
           child: Center(
             child: Column(
               children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note1.wav');
-                    },
-                    child: Container(
-                      color: Colors.red,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note2.wav');
-                    },
-                    child: Container(
-                      color: Colors.orange,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note3.wav');
-                    },
-                    child: Container(
-                      color: Colors.yellow,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note4.wav');
-                    },
-                    child: Container(
-                      color: Colors.green,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note5.wav');
-                    },
-                    child: Container(
-                      color: Colors.lightBlueAccent,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note6.wav');
-                    },
-                    child: Container(
-                      color: Colors.indigo,
-
-//                        AudioCache player = AudioCache();
-//                        player.play('note1.wav');
-                    ),
-                  ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: InkWell(
-                    onTap: () {
-                      AudioCache player = AudioCache();
-                      player.play('note7.wav');
-                    },
-                    child: Container(
-                      color: Colors.purple,
-
-                    ),
-                  ),
-                ),
+                xylo(),
+                xylo(),
+                xylo(),
+                xylo(),
+                xylo(),
+                xylo(),
+                xylo(),
               ],
             ),
           ),
